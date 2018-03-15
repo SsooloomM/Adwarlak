834be2d3b6185058e7a0e4ce12b665e95c3a0810
MAlDibawy <34455650+MAlDibawy@users.noreply.github.com>
2018-03-15 13:27:31 +0200

    maven
    mknsh sha8al f 3mlt al7agat dy...
    3shan ana gamed gedn. :D

--- src/main/java/com/SWEProject/Entities/Store.java
+++ src/main/java/com/SWEProject/Entities/Store.java
@@ -12,7 +12,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 
 @Entity
 public class Store {
