0df941e27ad8276c5bfe6cf78d61309c739f1a24
shroukksobhy <shroukksobhy@gmail.com>
2018-04-23 09:13:07 +0200

    User Controller
    add product to store
    delete store
    show products /*done*/

--- src/main/java/com/SWEProject/Controller/StoreController.java
+++ src/main/java/com/SWEProject/Controller/StoreController.java
@@ -5,19 +5,26 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.SWEProject.Entities.User;
-import com.SWEProject.Entities.StoreOwner;
+import com.SWEProject.Entities.Product;
 import com.SWEProject.Entities.Store;
+import com.SWEProject.Entities.StoreOwner;
+import com.SWEProject.Entities.StoreProducts;
+import com.SWEProject.Entities.User;
+import com.SWEProject.Repositories.ProductRepository;
+import com.SWEProject.Repositories.StoreProductRepository;
 import com.SWEProject.Repositories.StoreRepository;
 
-@Controller
+@RestController
+@CrossOrigin(origins = "http://localhost:8080")
 public class StoreController {
 
 	@Autowired
@@ -53,5 +60,44 @@ public class StoreController {
 		model.addAttribute("stores", SR.findAll());
 		return "showAllStores";
 	}
+	
+	@Autowired
+	private ProductRepository PR;
+	
+	@RequestMapping("/showProducts")
+	public List<Product> showProducts() {
+		List<Product> products=(List<Product>) PR.findAll();
+		if(products.size()==0)
+			return null;
+		return products;
+	}
+	
+	@RequestMapping("/deleteProduct")
+	public void deleteProduct(@RequestBody Product product) {
+		List<Product> productList=PR.findByName(product.getName());
+		if(productList.isEmpty()) return;
+		PR.delete(product);
+	}
+	
+	@Autowired
+	private StoreProductRepository SPR;
+	
+	@RequestMapping("/addProductToStore")
+	private Product addProductToStore1(@RequestBody Product product) { //not sure  store wla storeproduct	
+		List<StoreProducts> productlist=SPR.findByid(product.getId());
+		if(!productlist.isEmpty()) return null;
+		SPR.save(product);
+		return product;
+	}
+	
+
+	@RequestMapping("/addProductToStore")
+	private Product addProductToStore(@RequestBody Product product) { //not sure  store wla storeproduct	
+		List<Store> productlist=SR.findByName(product.getName());
+		if(!productlist.isEmpty()) return null;
+		SR.save(product);
+		return product;
+	}
+	
 	
 }
